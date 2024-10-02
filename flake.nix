453aabf638cd6d5c42bf876af04bd1c253a3e584
NullCube <nullcub3@gmail.com>
2024-10-02 02:09:32 -0700

    Flake: Get version from pom.xml file

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
         yesboats = pkgs.callPackage
           ({ lib, maven, jdk17_headless }: maven.buildMavenPackage rec {
             pname = "YesBoats";
-            version = "1.5.3";
+            version = with builtins; elemAt (match ".*${pname}</artifactId>[[:s:]]*<version>([[:d:]]{1,2}\.[[:d:]]{1,2}\.[[:d:]]{1,2}).*" (readFile ./pom.xml)) 0;
 
             src = ./.;
 
