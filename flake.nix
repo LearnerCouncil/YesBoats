0966e94788a2112aed3960f6548bf94c81ccffe0
NullCube <nullcub3@gmail.com>
2025-07-13 21:32:40 -0700

    Flake: Migrate to flake-parts
    Flake: Update nixpkgs to nixos-25.05

--- flake.nix
+++ flake.nix
@@ -2,58 +2,74 @@
   description = "Flake to build and develop YesBoats";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... } @inputs:
-    let
-      forAllSystems = function:
-        nixpkgs.lib.genAttrs [
+  outputs =
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { ... }:
+      {
+        systems = [
           "x86_64-linux"
           "aarch64-linux"
           "x86_64-darwin"
           "aarch64-darwin"
-        ]
-          (system: function (import nixpkgs { inherit system; }));
-    in
-    {
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            maven
-            jdk17
-          ];
-        };
-      });
+        ];
 
-      packages = forAllSystems (pkgs: rec {
-        default = yesboats;
-        yesboats = pkgs.callPackage
-          ({ lib, maven, jdk17_headless }: maven.buildMavenPackage rec {
-            pname = "YesBoats";
-            version = with builtins; elemAt (match ".*${pname}</artifactId>[[:s:]]*<version>([[:d:]]{1,2}\.[[:d:]]{1,2}\.[[:d:]]{1,2}).*" (readFile ./pom.xml)) 0;
+        perSystem =
+          { config, pkgs, ... }:
+          {
+            formatter = pkgs.nixfmt-tree;
 
-            src = ./.;
-
-            mvnHash = "sha256-EnSZWFqYUHvfqE8Sbr/BFEXsRoyyf3gb0JPJZVsB8zw=";
-
-            mvnJdk = jdk17_headless;
-            nativeBuildInputs = [
-              jdk17_headless
-            ];
-
-            installPhase = ''
-              mkdir -p $out/share/${pname}
-              install -Dm644 target/${pname}-${version}.jar $out/share/${pname}
-            '';
-
-            meta = {
-              description = "An ice boat racing plugin";
-              homepage = "https://github.com/LearnerCouncil/YesBoats";
-              license = lib.licenses.gpl3;
+            devShells.default = pkgs.mkShell {
+              packages = with pkgs; [
+                maven
+                jdk17
+              ];
             };
-          })
-          { };
-      });
-    };
+
+            packages = {
+              default = config.packages.yesboats;
+              yesboats = pkgs.callPackage (
+                {
+                  lib,
+                  maven,
+                  jdk17_headless,
+                }:
+                maven.buildMavenPackage rec {
+                  pname = "YesBoats";
+                  version =
+                    with builtins;
+                    elemAt (match ".*${pname}</artifactId>[[:s:]]*<version>([[:d:]]{1,2}\.[[:d:]]{1,2}\.[[:d:]]{1,2}).*" (readFile ./pom.xml)) 0;
+
+                  src = ./.;
+
+                  mvnHash = "sha256-wLsyvNLluSDCMynIW4R8Ym4Drtv728y0VaD+e/8UhrU=";
+
+                  mvnJdk = jdk17_headless;
+                  nativeBuildInputs = [
+                    jdk17_headless
+                  ];
+
+                  installPhase = ''
+                    mkdir -p $out/share/${pname}
+                    install -Dm644 target/${pname}-${version}.jar $out/share/${pname}
+                  '';
+
+                  meta = {
+                    description = "An ice boat racing plugin";
+                    homepage = "https://github.com/LearnerCouncil/YesBoats";
+                    license = lib.licenses.gpl3;
+                  };
+                }
+              ) { };
+            };
+          };
+      }
+    );
 }
